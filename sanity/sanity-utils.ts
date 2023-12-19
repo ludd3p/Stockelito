1e34c94b270b6a9316d890d42ac5b9fe5f43c9e3
ludd3p <77840976+ludd3p@users.noreply.github.com>
2023-12-19 15:52:05 +0100

    Function to get businesses and sorting the results.

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -3,11 +3,24 @@ import { client } from "./lib/client";
 
 export async function getNewsPosts () {
     return client.fetch(
-        groq`*[_type == 'news']{
+        groq`*[_type == 'news'] | order(_updatedAt desc){
             _id,
             _createdAt,
+            _updatedAt,
             newsTitle,
             newsText,
+        }`
+    )
+}
+
+export async function getBusiness () {
+    return client.fetch(
+        groq`*[_type == 'business'] | order(_updatedAt desc){
+            _id,
+            _createdAt,
+            updatedAt,
+            businessName,
+            businessText,
         }`
     )
 }

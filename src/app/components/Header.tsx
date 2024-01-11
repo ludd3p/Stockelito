0458ac603fff7c507070ac76a2c4434b19d754ab
ludd3p <77840976+ludd3p@users.noreply.github.com>
2024-01-11 11:29:40 +0100

    Update header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import DarkModeButton from "./DarkModeButton";
 
 const Header = () => {
   return (
     <header className="px-5 py-2 z-10 w-full flex items-center justify-between font-mono text-sm lg:flex-wrap">
       <h1>
-        STOCKELITO
+        <Link href={'/'}>STOCKELITO</Link>
       </h1>
       <DarkModeButton />
     </header>

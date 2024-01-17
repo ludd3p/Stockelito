2c655495f5d04dfd142e215b46514d599b631025
ludd3p <77840976+ludd3p@users.noreply.github.com>
2024-01-17 14:32:52 +0100

    More responsiveness

--- src/app/components/TopPart/TopPartItem.tsx
+++ src/app/components/TopPart/TopPartItem.tsx
@@ -5,7 +5,7 @@ const TopPartItem = (business: Business) => {
   return (
     <Link href={`/business/${business.slug}`}
       key={business._id}
-      className="flex flex-col items-center justify-center text-center rounded-full overflow-hidden border h-20 w-20 sm:h-28 sm:w-28 transition-colors
+      className="flex flex-col items-center justify-center text-center rounded-full overflow-hidden border h-16 w-16 xs:h-[4.5rem] xs:w-[4.5rem] sm:h-28 sm:w-28 transition-colors
                hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
       <h1 className="text-lg sm:text-2xl font-semibold">
         {business.businessTicker}

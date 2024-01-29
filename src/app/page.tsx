c22b22e8acc69f9b4e8710db2718ed4b9558cfd8
ludd3p <77840976+ludd3p@users.noreply.github.com>
2024-01-29 15:21:43 +0100

    Split page for static rendering and pre fetching data,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@
 import TopPart from "./components/TopPart/TopPart"
 import News from "./components/News/News"
 import Horoscope from "./components/Horoscope/Horoscope"
+
 export default function Home() {
+ 
   return (
     <div className="flex flex-col items-center py-10 w-full max-w-7xl mx-auto">
         <TopPart />

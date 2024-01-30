4a8d4a9767a3bdec621fd396444622149d3d9cf5
ludd3p <77840976+ludd3p@users.noreply.github.com>
2024-01-30 22:03:48 +0100

    New more basic scraper method

--- data/scraper.ts
+++ data/scraper.ts
@@ -1,5 +1,5 @@
 import puppeteer from 'puppeteer';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import pMap from 'p-map';
 import { getBusinesses } from '../sanity/sanity-utils';
 import { Business } from '../Types/SanityTypes';
@@ -12,49 +12,76 @@ export interface MarketDataItem {
   }[];
 }
 
+
+// PITA to deploy on vercel
 export async function scrapeMarketData(slug: string): Promise<MarketDataItem> {
-  const browser = await puppeteer.launch({ headless: "new", args: ['--no-sandbox'],});
+  const browser = await puppeteer.launch({ headless: "new", args: ['--no-sandbox'], });
   const page = await browser.newPage();
   const query = slug;
 
   try {
-    // Navigate to the search results page
-    await page.goto(`https://www.di.se/search/?query=${query}`);
+    await page.goto(process.env.DI_ADDRESS + query);
 
-    // Wait for the search results to load
     await page.waitForSelector('body > div.js_site-wrapper.site-wrapper.panorama-is-scrolled-out > div.js_main-content.site-body > div.search > section:nth-child(2) > div > table', { timeout: 10000 });
 
-    // Click the link in the first <td> in the first <tr> of the table
     await page.click('body > div.js_site-wrapper.site-wrapper.panorama-is-scrolled-out > div.js_main-content.site-body > div.search > section:nth-child(2) > div > table tbody tr:first-child td:first-child a');
 
-    // Wait for the market data section to load
     await page.waitForSelector('body > div.js_site-wrapper.site-wrapper.panorama-is-scrolled-out > div.js_main-content.site-body > div > div.market__content > div.market__content-main > article:nth-child(4) > section');
 
-    // Use Cheerio to parse the HTML of the targeted section
     const sectionContent = await page.$eval('body > div.js_site-wrapper.site-wrapper.panorama-is-scrolled-out > div.js_main-content.site-body > div > div.market__content > div.market__content-main > article:nth-child(4) > section',
       (section) => section.innerHTML
     );
 
     const $ = cheerio.load(sectionContent);
 
-    // Example: Extract data from each item in the section
     const data: { title: string, value: string }[] = [];
 
     $('div.instrument-info__item').each((index, element) => {
       const title = $(element).find('h3').text();
       const value = $(element).find('p, a').text();
 
-      // Add the extracted data to the array
       data.push({ title, value });
     });
 
     console.log(`klar med en ${slug}`);
-
-    // Return the structured data
     return { slug, data };
   } finally {
-    // Close the browser
     await browser.close();
+  }
+}
+
+export async function scrapeBusinessByURL(URL: string): Promise<MarketDataItem> {
+    const page = await fetch(URL);
+    const html = await page.text();
+
+    const $ = await cheerio.load(html);
+
+    const sectionContent = $('body').html();
+
+    const data: { title: string, value: string }[] = [];
+    if (sectionContent) {
+      const section$ = cheerio.load(sectionContent);
+
+      section$('div.instrument-info__item').each((index, element) => {
+        const title = section$(element).find('h3').text();
+        const value = section$(element).find('p, a').text();
+
+        data.push({ title, value });
+      });
+    }
+    return { slug: URL, data };
+}
+
+
+
+
+export async function scrapeBusiness(slug: string): Promise<MarketDataItem> {
+  try {
+    return await scrapeBusinessByURL(slug);
+  }
+  catch (error) {
+    console.error('Error:', error);
+    return { slug, data: [] };
   }
 }
 
@@ -78,14 +105,4 @@ export async function runScrapingForAllBusinesses(): Promise<MarketDataItem[]> {
     console.error('Error:', error);
     throw error;
   }
-}
-
-export async function scrapeBusiness(slug: string): Promise<MarketDataItem> {
-  try {
-    return await scrapeMarketData(slug);
-  }
-  catch (error) {
-    console.error('Error:', error);
-    return { slug, data: [] };
-  } 
 }

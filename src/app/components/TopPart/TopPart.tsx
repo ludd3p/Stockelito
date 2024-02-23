a1c433245a769e8b4942f574506d6f6d80132b90
ludd3p <77840976+ludd3p@users.noreply.github.com>
2024-02-23 15:04:58 +0100

    Update business links

--- src/app/components/TopPart/TopPart.tsx
+++ src/app/components/TopPart/TopPart.tsx
@@ -19,21 +19,24 @@ const TopPart = () => {
     const itemsForRow3 = business.slice(9, 14);
 
     return (
-        <div className="w-full items-center">
-            <div className="grid text-center w-full  grid-cols-5 place-items-center">
-                {itemsForRow1.map((business: Business) => (
-                    <TopPartItem key={business._id} {...business} />
-                ))}
-            </div>
-            <div className="grid text-center w-full  grid-cols-4 place-items-center px-[10%]">
-                {itemsForRow2.map((business: Business) => (
-                    <TopPartItem key={business._id} {...business} />
-                ))}
-            </div>
-            <div className="grid text-center w-full grid-cols-5 place-items-center">
-                {itemsForRow3.map((business: Business) => (
-                    <TopPartItem key={business._id} {...business} />
-                ))}
+        <div className="w-full">
+            <h1 className="w-fit ml-[5%] text-3xl font-bold tracking-tighter sm:text-4xl lg:text-5xl/none">Våra favoriter</h1>
+            <div className="items-center">
+                <div className="grid text-center w-full  grid-cols-5 place-items-center">
+                    {itemsForRow1.map((business: Business) => (
+                        <TopPartItem key={business._id} {...business} />
+                    ))}
+                </div>
+                <div className="grid text-center w-full  grid-cols-4 place-items-center px-[10%]">
+                    {itemsForRow2.map((business: Business) => (
+                        <TopPartItem key={business._id} {...business} />
+                    ))}
+                </div>
+                <div className="grid text-center w-full grid-cols-5 place-items-center">
+                    {itemsForRow3.map((business: Business) => (
+                        <TopPartItem key={business._id} {...business} />
+                    ))}
+                </div>
             </div>
         </div>
     )

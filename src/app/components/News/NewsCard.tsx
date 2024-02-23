60ce625a9bb20c0ea1f3115bf533b972654fd63b
ludd3p <77840976+ludd3p@users.noreply.github.com>
2024-02-23 15:04:26 +0100

    Card update 2

--- src/app/components/News/NewsCard.tsx
+++ src/app/components/News/NewsCard.tsx
@@ -17,16 +17,15 @@ const NewsCard = (newsItem: NewsItem) => {
   return (
     <div className="flex justify-center items-center w-full h-full">
       <Link href={`/news/${newsItem.slug}`} className="flex flex-col group relative rounded-xl 
-        px-6 pt-6 pb-2 h-80 w-80 lg:h-90 lg:w-45 overflow-hidden cursor-pointer justify-between
+        px-2 pt-4 pb-2 h-80 w-80 lg:h-90 lg:w-45 overflow-hidden cursor-pointer justify-between
         transition hover:scale-105">
-        <div className="absolute inset-0 bg-cover bg-center" />
         <div className="absolute inset-0 bg-black opacity-10 dark:bg-white group-hover:opacity-20" />
         <div className="relative flex flex-col gap-3">
-          <h1 className="text-xl lg:text-2xl">{newsItem.newsTitle}</h1>
+          <h1 className="text-xl lg:text-2xl mx-auto">{newsItem.newsTitle}</h1>
           <p className="lg:text-l">{newsItem.newsText}</p>
         </div>
         <div>
-          <p>Skrivet {formattedDate} kl. {formattedTime}</p>
+          <p className="ml-auto text-sm w-fit text-gray-500">Skrivet {formattedDate} kl. {formattedTime}</p>
         </div>
       </Link>
     </div>

f1e1051c008f63b923edeef72a539deafe482c8a
ludd3p <77840976+ludd3p@users.noreply.github.com>
2024-02-26 14:37:25 +0100

    Added användarvillkor

---
+++ src/app/anvandarvillkor/page.tsx
@@ -0,0 +1,41 @@
+import './styles.css';
+
+export default function anvandarvillkor() {
+    return (
+        <div className="flex flex-col py-14 w-full max-w-7xl mx-auto">
+            <div className="max-w-3xl space-y-4 mx-auto md:w-[90%] lg:mx-14">
+                <h1>Användarvillkor för Stockelito</h1>
+                <p>Välkommen till Stockelito! Tack för att du besöker vår hemsida. Innan du använder vår tjänst, vänligen läs igenom dessa användarvillkor noggrant. Genom att använda vår hemsida samtycker du till att följa och vara bunden av dessa villkor. Om du inte godkänner något av villkoren, vänligen lämna vår hemsida.</p>
+                <div>
+                    <h2>Tjänsteleverantör och Syfte:</h2>
+                    <p>Stockelito (nedan kallad "vi", "oss" eller "vår") är en onlineplattform som tillhandahåller analyser, intervjuer, rykten och spekulationer om olika företag, särskilt mindre bolag som kanske inte är välkända för allmänheten. Vår tjänst är avsedd att erbjuda användare information och insikter om dessa företag.</p>
+                </div>
+                <div>
+                    <h2>Användning av Tjänsten:</h2>
+                    <p>Genom att använda vår hemsida samtycker du till att inte missbruka tjänsten genom att sprida desinformation, olagligt material eller på annat sätt störa våra tjänster eller andra användares upplevelser.</p>
+                </div>
+                <div>
+                    <h2>Ansvarsfriskrivning:</h2>
+                    <p>Informationen som tillhandahålls på vår hemsida är endast för informationsändamål och bör inte betraktas som professionell rådgivning eller rekommendation. Vi friskriver oss från ansvar för eventuella förluster eller skador som uppstår till följd av användningen av informationen på vår hemsida.</p>
+                </div>
+                <div>
+                    <h2>Användaruppgifter:</h2>
+                    <p>För närvarande samlar vi inte in några personuppgifter från våra användare. Vi följer en strikt integritetspolicy och kommer att uppdatera användarna om några ändringar i vår policy för insamling och användning av användaruppgifter.</p>
+                </div>
+                <div>
+                    <h2>Immateriella rättigheter:</h2>
+                    <p>Allt innehåll som publiceras på vår hemsida, inklusive text, bilder, grafik, logotyper och annat material, är skyddat av upphovsrättslagar och andra tillämpliga lagar om immateriella rättigheter. Användare får inte reproducera, distribuera eller använda detta material utan tillstånd från oss.</p>
+                </div>
+                <div>
+                    <h2>Ändringar av Användarvillkor:</h2>
+                    <p>Vi förbehåller oss rätten att när som helst uppdatera eller ändra dessa användarvillkor. Det är användarens ansvar att regelbundet granska villkoren för eventuella ändringar. Fortsatt användning av vår hemsida efter publicering av ändringar innebär godkännande av de reviderade villkoren.</p>
+                </div>
+                <div>
+                    <h2>Kontaktinformation:</h2>
+                    <p>För frågor eller bekymmer angående dessa användarvillkor eller vår hemsida, vänligen kontakta oss på info@stockelito.se.</p>
+                </div>
+                <p>Genom att använda vår hemsida godkänner du dessa användarvillkor och förbinder dig att följa dem under hela din användning av tjänsten. Tack för ditt förtroende och ditt stöd för Stockelito!</p>
+            </div>
+        </div>
+    )
+}

e409c018a0d9418183c27278a276b595ec87b472
Simon Gärtner <simon.gartner@positiongreen.com>
2024-07-29 19:57:19 +0200

    Added Hero image

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Hero() {
     return (
-        <section className="w-full py-16" style={{ position: "absolute", zIndex: -1, height: "100vh", backgroundImage: "url('https://s3-alpha-sig.figma.com/img/77ee/f37a/d12753127ede0839586069f34352ffbc?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=IvC9TD5wyWQ2yYO1ZF3fnRh286FsNmukR8LMrpErLa2crKQnTlKG7K06MrqcRXwn5FILSk3I9Ie84TCvn-85zDug5ius7M8EZWzNC4vXLYCpGsFuut6Hvi6GQ7ntDrbHg9mWUtlCpPX37ZmUrKBdUr5IWnqdAIqyvxXHfdcQunM~AoPZ5Gky9YchkDRkBNDvY-osZCqyAjiasRN9rlVW5OtGBpBYrarV22IsFZwDKEhekCsZwi405fmUZZ7MEcpzEINXigKCcKqi0nzE5kOnIhwyvv6PmmOsGwL6DvZRg7v~huWz1PtCxInmgALBlz-p48BmJ7x4RLd58sI4-Vu~EQ__')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "bottom" }}>
+        <section className="w-full py-16" style={{ position: "absolute", zIndex: -1, height: "100vh", backgroundImage: "url('/hero.png')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "bottom" }}>
         </section>
     );
 }

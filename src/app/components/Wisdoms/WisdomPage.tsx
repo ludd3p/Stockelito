a27d66e66e07ad3599be17e91f4586b1d046084a
Simon Gärtner <simon.gartner@positiongreen.com>
2024-09-29 19:18:27 +0200

    New take on cards wisdoms

--- src/app/components/Wisdoms/WisdomPage.tsx
+++ src/app/components/Wisdoms/WisdomPage.tsx
@@ -13,41 +13,41 @@ export const WisdomPage = ({ wisdoms }: WisdomPageProps) => {
                 <div className="flex flex-wrap justify-start -mx-4">
                     {wisdoms.map((wisdom: NewsItem) => (
                         <div
-                            key={wisdom._id}
-                            className="w-full sm:w-1/2 lg:w-1/3 p-4 flex justify-center"
+                        key={wisdom._id}
+                        className="w-full sm:w-1/2 lg:w-1/3 p-4 flex justify-center"
+                      >
+                        <div
+                          className="bg-white p-6 rounded-lg shadow-lg w-80 h-96 flex flex-col relative"
+                          style={{
+                            backgroundImage: 'url("/tarot-card-light.svg")',
+                            backgroundSize: 'contain', // Ensures the image covers the entire div
+                            backgroundPosition: 'center', // Centers the image within the div
+                          }}
                         >
-                            <div className="bg-white p-6 rounded-lg shadow-lg w-80 h-96 flex flex-col relative">
-                                {/* Top-left image */}
-                                <div className="absolute -top-4 -left-4 w-10 h-10">
-                                    <Image width="40" height="40" alt="corner image 1" src="tarot-card-dark.svg" />
-                                </div>
-
-                                {/* Top-right image */}
-                                <div className="absolute -top-4 -right-4 w-10 h-10">
-                                    <Image width="40" height="40" alt="corner image 2" src="tarot-card-dark.svg" />
-                                </div>
-
-                                {/* Bottom-left image */}
-                                <div className="absolute -bottom-4 -left-4 w-10 h-10">
-                                    <Image width="40" height="40" alt="corner image 3" src="tarot-card-dark.svg" />
-                                </div>
-
-                                {/* Bottom-right image */}
-                                <div className="absolute -bottom-4 -right-4 w-10 h-10">
-                                    <Image width="40" height="40" alt="corner image 4" src="tarot-card-dark.svg" />
-                                </div>
-
-                                <h2 className="text-2xl font-bold mb-4 text-black">{wisdom.newsTitle}</h2>
-                                <p className="text-black flex-grow">{wisdom.newsText}</p>
-
-                                {/* Check icon if fulfilled */}
-                                {wisdom.isFulfilled && (
-                                    <div style={{ width: "30px", height: "30px", position: "absolute", top: -15, right: -15 }}>
-                                        <Image width="30" height="30" alt="noreferer" src="/check-icon.png" />
-                                    </div>
-                                )}
+                          <h2 className="text-2xl font-bold mb-4 text-black">{wisdom.newsTitle}</h2>
+                          <p className="text-black flex-grow">{wisdom.newsText}</p>
+                      
+                          {/* Check icon if fulfilled */}
+                          {wisdom.isFulfilled && (
+                            <div
+                              style={{
+                                width: "30px",
+                                height: "30px",
+                                position: "absolute",
+                                top: -15,
+                                right: -15,
+                              }}
+                            >
+                              <Image
+                                width="30"
+                                height="30"
+                                alt="check icon"
+                                src="/check-icon.png"
+                              />
                             </div>
+                          )}
                         </div>
+                      </div>
                     ))}
                 </div>
             </div>

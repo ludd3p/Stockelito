a3fa8c169dcf2bb5a6ff5866f5c96e32f40edee0
Simon Gärtner <simon.gartner@positiongreen.com>
2024-10-06 15:40:07 +0200

    FIx tarotcard

--- src/app/components/Wisdoms/WisdomCard.tsx
+++ src/app/components/Wisdoms/WisdomCard.tsx
@@ -14,19 +14,21 @@ export const WisdomCard = ({ wisdom }: { wisdom: NewsItem }) => {
         minute: '2-digit',
     });
     return (
-
         <div
             key={wisdom._id}
             className="w-full sm:w-1/2 lg:w-1/3 p-4 flex justify-center"
         >
             <div
-                className="bg-white p-6 rounded-lg shadow-lg w-80 h-96 flex flex-col relative"
+                className="p-6 rounded-lg flex flex-col relative"
                 style={{
                     backgroundImage: 'url("/tarot-card-light.svg")',
-                    backgroundSize: 'contain', // Ensures the image covers the entire div
+                    backgroundSize: 'cover', // Ensures the image covers the entire div while maintaining its aspect ratio
                     backgroundPosition: 'center', // Centers the image within the div
                     textShadow: "-1px -1px 0 white, 1px -1px 0 white, -1px 1px 0 white, 1px 1px 0 white",
-
+                    width: '100%', // Make the card fill the entire width of its parent container
+                    height: "450px", // Set a fixed height, or adjust as necessary for your design
+                    maxWidth: '300px', // Ensures the card doesn’t get too large on bigger screens
+                    backgroundRepeat: 'no-repeat' // Prevents the background from repeating
                 }}
             >
                 <h2 className="text-2xl font-bold mb-4 text-black">{wisdom.newsTitle}</h2>
@@ -54,8 +56,6 @@ export const WisdomCard = ({ wisdom }: { wisdom: NewsItem }) => {
                     </div>
                 )}
             </div>
-        </div >
-
-
+        </div>
     );
 };

780d975cea51fddb1bb1b54b395c17d49147cd03
Simon Gärtner <simon.gartner@positiongreen.com>
2024-11-02 11:15:16 +0100

    Delete exposed key and change setup.

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn, token } from '../env'
+import { apiVersion, dataset, projectId, useCdn } from '../env'
+import { token } from '../env'
 
 export const client = createClient({
   apiVersion,

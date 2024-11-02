f7ba4be37fc46d1175eaba5a2cd6904e1241e140
Simon Gärtner <simon.gartner@positiongreen.com>
2024-11-02 11:24:24 +0100

    Fix

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,7 +1,6 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
-import { token } from '../env'
+import { apiVersion, dataset, projectId, useCdn,token } from '../env'
 
 export const client = createClient({
   apiVersion,
